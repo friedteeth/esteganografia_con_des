6ab82e44e142740a3a3efa01b57c6c9e756e4b3e
Salvador Loera <loeraquirozs@gmail.com>
2020-06-15 05:58:40 -0500

    Se creo el proyecto de django

---
+++ esteganografia_des/esteganografia_des/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for esteganografia_des project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esteganografia_des.settings')
+
+application = get_asgi_application()
