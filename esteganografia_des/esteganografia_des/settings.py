5088fde1d03d43325e0e097d927443eb65d7545f
Salvador Loera <loeraquirozs@gmail.com>
2020-06-15 23:17:28 -0500

    Se configuro settings.py y se agrego bootstrap

--- esteganografia_des/esteganografia_des/settings.py
+++ esteganografia_des/esteganografia_des/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x@bp7&go6)=kpm85bqykbaj23frr8ej-k2-f07=dogr(3s(t$k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
