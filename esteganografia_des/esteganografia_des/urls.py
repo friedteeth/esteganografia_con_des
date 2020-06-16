e12c105a82ebed55f2ae62c7b172ba8043f403fc
Salvador Loera <loeraquirozs@gmail.com>
2020-06-16 12:47:43 -0500

    Semejoro la vista, el form y la view

--- esteganografia_des/esteganografia_des/urls.py
+++ esteganografia_des/esteganografia_des/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ocultar, revelar
+from esteganografia.views import ocultar, revelar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
